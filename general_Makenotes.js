559e14ec08d63c3ee1ea91d7b62574f2476163a4
JulioCod <juliocodesal@gmail.com>
2025-01-30 22:17:24 +0100

    Update general_Makenotes.js

--- general_Makenotes.js
+++ general_Makenotes.js
@@ -32,7 +32,7 @@ class general_Makenotes {
         if (!word) return [];
         let notes = [];
         let css = '<style>.odh-expression {font-size: 1em!important;font-weight: normal!important;}</style>';
-        notes.push({ css, 'Hello world': [this.makenotes_lable] });
+        notes.push({ css, 'Hello world': ['Me da igual'] });
         notes.push({ css, definitions: ['Hola mundo'] });
         return notes;
     }
