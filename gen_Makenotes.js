51d05be5ef661dfa514a4bc150fbfe46f10ae7a3
JulioCod <juliocodesal@gmail.com>
2025-01-30 21:18:25 +0100

    Update gen_Makenotes.js

--- gen_Makenotes.js
+++ gen_Makenotes.js
@@ -8,7 +8,7 @@ class gen_Makenotes {
     }
 
     async displayName() {
-        let locale = await api.locale();
+        let locale = await api.locale()
         if (locale.indexOf('CN') != -1) { this.makenotes_lable = '输入笔记(需在后台配置输出选项以保存)'; return '笔记摘录脚本'; }
         if (locale.indexOf('TW') != -1) { this.makenotes_lable = '輸入筆記(需在後臺配置輸出選項以保存)'; return '筆記摘錄腳本'; }
         this.makenotes_lable = 'add notes here.(to save, need setup export option in backend)';
@@ -23,9 +23,36 @@ class gen_Makenotes {
 
     async findTerm(word) {
         this.word = word;
-        //let deflection = api.deinflect(word);
-        let results = await Promise.all([this.makeNotes(word)]);
-        return [].concat(...results).filter(x => x);
+        return await this.findMiCodigo(word);
+    }
+
+    async findMiCodigo(word) {
+        if (!word) return null;
+
+        let base = 'https://dictionary.cambridge.org/dictionary/english-spanish/';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return null;
+        }
+
+        let contents = doc.querySelectorAll('.def-body') || [];
+        if (contents.length == 0) return null;
+
+        let definition = '';
+        for (const content of contents) {
+            this.removeTags(content, '.bubble--3j0Ro');
+            this.removeTags(content, '.copyright--2TbNS');
+            this.removelinks(content);
+            //definition += content.innerHTML;
+            definition += 'Hola mundo'
+        }
+        let css = this.renderCSS();
+        return definition ? css + definition : null;
     }
 
     async makeNotes(word) {
